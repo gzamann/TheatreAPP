4aa9e5ecfa78fc54c2da9090e1887835b4c02478
aesthetism <zman97619@gmail.com>
2018-10-01 11:14:09 +0530

    newbranch

--- app/models/audis.rb
+++ app/models/audis.rb
@@ -3,6 +3,6 @@ class Audis < ApplicationRecord
   validates :seats, presence: true, numericality:{only_integer: true}
   has_many :shows
   has_many :tickets
-  has_many :movies, through :shows
+  has_many :movies, through: :shows
   belongs_to :theatre
 end
