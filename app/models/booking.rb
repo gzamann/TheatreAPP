91a6d079de4d5ba183003223b106ca97d74b8f80
aesthetism <zman97619@gmail.com>
2018-10-31 20:26:41 +0530

    cmt

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,25 +1,23 @@
 class Booking < ApplicationRecord
   validates :name, presence: true, length:{maximum:30}
   validates :seats, presence: true, numericality:{only_integer: true}, length:{maximum:5}
+  validate :seats_book
   has_many :tickets , dependent: :destroy
   belongs_to :show
-
-  before_update :seats_book
-  after_create :seats_cal
+  
+  after_commit :seats_cal, :on(:create, :update)
   
   private
     def seats_book
-      if seats > Show.seats
+      if self.seats > self.show.seats
         errors.add(:seats, "Not enough seats available.")
       end
     end
 
     def seats_cal
-      @show = Show.find(params[:show_id])
-      @seats = Booking.find(params[:seats])
+      @show = Show.find_by(:id)
+      @seats = Booking.find
       @show.seats -= @seats
-      
-      Shows.save
     end
     
 end
